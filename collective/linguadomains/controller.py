0794bec5842ea7333e0e4b5bd923088ff0015ded
Lur Ibargutxi <libargutxi@codesyntax.com>
2016-09-27 10:01:10 +0200

    static

--- collective/linguadomains/controller.py
+++ collective/linguadomains/controller.py
@@ -88,6 +88,8 @@ class LinguaDomainsManager(object):
     def get_translated_url(self):
 
         url = self.request.get('ACTUAL_URL')
+        if "@@deploy-custom-static" in url:
+            return url
         purl = self.portal_url()
         lang = self.language()
 
